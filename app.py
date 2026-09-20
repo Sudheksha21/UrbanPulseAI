5300d79033e793e60f36fd6790574c8e298537e3
Sudheksha21 <sudheksharj575@gmail.com>
2026-09-20 13:02:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ st.set_page_config(
 def load_model():
 
     model_package = joblib.load(
-        "traffic_model.pkl"
+        "models/traffic_model.pkl"
     )
 
     model = model_package["model"]
